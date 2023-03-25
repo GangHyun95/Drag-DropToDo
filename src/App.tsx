29003ab6731a15bd617075e10775b6888c471178
hyun <hgh6128@gmail.com>
2023-03-25 23:42:55 +0900

    CSS 스타일링

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,69 @@
 import React from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import styled from "styled-components";
 // DragDopContext로 사용할 컴포넌트 감싸줌
 // onDragEnd 함수는 유저가 드래그를 끝낸 시점ㅈ에 불려지는 함수
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 480px;
+  height: 100vh;
+  margin: 0 auto;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(1, 1fr);
+  min-height: 200px; //없어도됨
+`;
+const Board = styled.ul`
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+`;
+
+const Card = styled.li`
+  padding: 10px;
+  margin-bottom: 4px;
+  background-color: ${(props) => props.theme.cardColor};
+  border-radius: 5px;
+`;
+
+const toDos = ["a", "b", "c", "d", "e", "f"];
+
 function App() {
   const onDragEnd = () => {};
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <div>
-        <Droppable droppableId="one">
-          {(magic) => (
-            <ul ref={magic.innerRef} {...magic.droppableProps}>
-              <Draggable draggableId="first" index={0}>
-                {(magic) => (
-                  <li ref={magic.innerRef} {...magic.draggableProps}>
-                    <span {...magic.dragHandleProps}>💥</span>
-                    One
-                  </li>
-                )}
-              </Draggable>{" "}
-              <Draggable draggableId="seoncd" index={1}>
-                {(magic) => (
-                  <li ref={magic.innerRef} {...magic.draggableProps}>
-                    {" "}
-                    <span {...magic.dragHandleProps}>💥</span>
-                    Two
-                  </li>
-                )}
-              </Draggable>
-            </ul>
-          )}
-        </Droppable>
-      </div>
+      <Wrapper>
+        <Boards>
+          <Droppable droppableId="one">
+            {(magic) => (
+              <Board ref={magic.innerRef} {...magic.droppableProps}>
+                {toDos.map((todo, i) => (
+                  <Draggable draggableId={todo} index={i}>
+                    {(magic) => (
+                      <Card
+                        ref={magic.innerRef}
+                        {...magic.draggableProps}
+                        {...magic.dragHandleProps}
+                      >
+                        {todo}
+                      </Card>
+                    )}
+                  </Draggable>
+                ))}
+                {magic.placeholder}
+              </Board>
+            )}
+          </Droppable>
+        </Boards>
+      </Wrapper>
     </DragDropContext>
   );
 }
