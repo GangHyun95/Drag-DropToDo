c943d6acb82490537ed4a0c6cb3d9b45265ba28f
hyun <hgh6128@gmail.com>
2023-03-25 23:09:20 +0900

    react-beautifil-dnd 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
 import React from "react";
-
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+// DragDopContext로 사용할 컴포넌트 감싸줌
+// onDragEnd 함수는 유저가 드래그를 끝낸 시점ㅈ에 불려지는 함수
 function App() {
-  return <div></div>;
+  const onDragEnd = () => {};
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId="one">
+          {(magic) => (
+            <ul ref={magic.innerRef} {...magic.droppableProps}>
+              <Draggable draggableId="first" index={0}>
+                {(magic) => (
+                  <li ref={magic.innerRef} {...magic.draggableProps}>
+                    <span {...magic.dragHandleProps}>💥</span>
+                    One
+                  </li>
+                )}
+              </Draggable>{" "}
+              <Draggable draggableId="seoncd" index={1}>
+                {(magic) => (
+                  <li ref={magic.innerRef} {...magic.draggableProps}>
+                    {" "}
+                    <span {...magic.dragHandleProps}>💥</span>
+                    Two
+                  </li>
+                )}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
 }
 
 export default App;
